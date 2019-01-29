44650148135037ae9963a71fe783ec43fb286cfe
Luis Giordano <14226200+h4cker39@users.noreply.github.com>
2019-01-29 16:31:19 -0200

    Create ex0.cs

---
+++ ex0.cs
@@ -0,0 +1,11 @@
+//Exercise 0 - 
+
+//Using the following atomic queries:
+//CxList.FindByType()
+//CxList.FindByShortName()
+
+//Find all method decleration that start with b
+
+CxList bType = All.FindByShortName("b*");
+CxList methodD = bType.FindByType(typeof(MethodDecl));
+result = methodD;
